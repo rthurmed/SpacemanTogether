925916d524fd86fac10afce8f415751a688088e3
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-06-13 11:35:34 -0300

    Added pause menu

---
+++ assets/font/README.md
@@ -0,0 +1,8 @@
+# Mousetrap
+
+mousetrap by jeti: A tiny font that manages to remain legible. Who would have thought?
+
+You are free to use this font for personal or commercial projects, all I ask is that you include credit.
+
+Licensed under CC BY 4.0: https://creativecommons.org/licenses/by/4.0/
+More info: https://fontenddev.com/fonts/mousetrap/
---
+++ src/system/Pause.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+
+onready var ui = $UI
+onready var btn_continue = $UI/VBoxContainer/Continue
+
+
+func _input(event):
+	if event.is_action_released("pause"):
+		paused(not get_tree().paused)
+
+
+func paused(state: bool):
+	get_tree().paused = state
+	ui.visible = state
+	
+	if ui.visible:
+		btn_continue.grab_focus()
+
+
+func _on_Continue_pressed():
+	paused(false)
+
+
+func _on_Restart_pressed():
+	paused(false)
+	get_tree().reload_current_scene()
